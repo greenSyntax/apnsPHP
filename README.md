75f30db594b9e8039c0def0b1febd52de82f3b05
greenSyntax <ab.abhishek.ravi@gmail.com>
2018-03-31 22:56:00 +0530

    Updated README and source

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# apnsPHP
-A PHP Library for Apple Push Notification Service
+## Apple Push Notification Service via PHP
+#### A PHP Library for Apple Push Notification Service
+
+### Features
+- [x] Send Push Notification
+- [x] Customise as JSON Payload
+
+### Installation
+
+You can install apnsPHP via Composer.
+First, add **composer.json** file in your project
+
+```json
+{
+  "require": {
+    	"apns/apnsPHP": "*"
+    }
+}
+```
+
+And, install dependencies by command
+
+```
+composer install
+```
+
+### Code
+
+1. Send Push Notification when you've text only
+
+```php
+
+$apns = new apnsPHP();
+$apns->sendPushNotification("This is my Message", $token);
+
+```
+
+2. Send Push Notification when you've complete JSON Payload
+
+As, Apple Push Notification payload looks like this where, **alert** is the push-notification message, **badge** is the badge number which comes on AppIcon in numeric form with red bubble and last is **sound** which is alert tone when push notification pops.
+
+```json
+{
+  aps:{
+    'alert' : 'Hello World :D',
+    'badge' : '1',
+    'sound' : 'default'
+  }
+}
+```
+
+So, you have to first prepare the payload and then call apns defined method,
+
+```php
+// Token
+$token = "FCFFA7C61D647BA62DAxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+
+// Associative Array
+$payload['aps'] = array('alert' => 'Hello World :D', 'badge' => 1, 'sound' => 'default');
+
+// Convert Array into JSON
+$payloadJSON = json_encode($payload);
+```
+
+And, last you have to call APNS method.
+
+```php
+
+$apns = new apnsPHP();
+$apns->sendPushNotification($payloadJSON, $token);
+
+```
+
+## Reference
+You can refer Apple [Documentation](https://developer.apple.com/library/content/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/APNSOverview.html) for Apple Push Notification Service.
--- src/apns/apnsPHP.php
+++ src/apns/apnsPHP.php
@@ -2,11 +2,71 @@
 
 namespace apns;
 
-class apnsPHP {
+public class apnsPHP {
 
-  public static function sendPushNotification() {
-    echo "Testing ...";
-  }
+  // Mode of APNS
+    private $mode = "DEVELOPMENT";
+    #private $mode = "PRODUCTION";
+
+    private $apnsPort = 2195;
+    private $pathOfCertificate;
+    private $payload;
+
+    function __construct($pathOfCertficate){
+      // Set APNS Cer Certificate
+      $this->pathOfCertficate = $pathOfCertficate;
+    }
+
+    private function getHostName(){
+
+      if($this->mode == "DEVELOPMENT"){
+        return "gateway.sandbox.push.apple.com";
+      }
+      else{
+        return "gateway.push.apple.com";
+      }
+    }
+
+    private function validate($payload, $token){
+
+      # This is Lame Logic. Kindly Improve this.
+      if($payload == null){
+        return "Payload is assigned";
+      }
+
+      if($token == null){
+        return "Token is not assigned";
+      }
+
+      if($this->pathOfCertficate == null){
+        return "APNS Certifcate is not assigned";
+      }
+
+      return true;
+    }
+
+    public function sendPushNotification($message, $token) {
+
+      $paylod = $payload['aps'] = array('alert' => $message, 'badge' => 1, 'sound' => 'default');
+      sendPushNotificationAsJSON(json_encode($payload), $token);
+    }
+
+    // Send Push Notification To Tokens
+    public function sendPushNotificationAsJSON($payload, $token){
+
+      if($this->validate($payload, $token) != true){
+        return "Validation Error";
+      }
+
+      //echo $this->pathOfCertficate;
+      $token = pack('H*', str_replace(' ', '', $token));
+      $apnsMessage = chr(0).chr(0).chr(32).$token.chr(0).chr(strlen($payload)).$payload;
+      $streamContext = stream_context_create();
+      stream_context_set_option($streamContext, 'ssl', 'local_cert', $this->pathOfCertficate);
+      $apns = stream_socket_client('ssl://'.$this->getHostName().':'.$this->apnsPort, $error, $errorString, 2, STREAM_CLIENT_CONNECT, $streamContext);
+      fwrite($apns, $apnsMessage);
+      fclose($apns);
+    }
 
 }
 
